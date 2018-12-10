8da7c98ba4f82b539f9522442a17317b4bb44977
Lndubose <lndubose13@gmail.com>
2018-12-09 23:57:53 -0600

    Setup React App and React Router

---
+++ reactdev/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
